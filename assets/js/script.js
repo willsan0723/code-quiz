48b10b817b888b28919d16e2fd0eec20ead4ff45
willsan0723 <william.santee@gmail.com>
2020-08-21 22:29:03 -0600

    expanded functionality of timer, setup initial page display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 var timerEl = document.getElementById('timer');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
+var timeInterval;
 
 function countdown() {
     var timeLeft = 75;
+    askQuestions();
     timerEl.textContent = ("Time: " + timeLeft);
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
+    clearInterval(timeInterval);
+    timeInterval = setInterval(function () {
         if (timeLeft > 1) {
             timerEl.textContent = ("Time: " + --timeLeft);
         }
@@ -31,34 +34,34 @@ var questions = [
         correctAnswer: 'c'
     },
     {
-        q: "Common data types do not include:",
+        q: "Which is not a looping structure in JavaScript?",
         answers: {
-            a: 'Boolean',
-            b: 'String',
-            c: 'Alert',
-            d: 'Number'
+            a: 'For',
+            b: 'While',
+            c: 'do-while',
+            d: 'If'
         },
-        correctAnswer: 'c'
+        correctAnswer: 'd'
     },
     {
-        q: "Common data types do not include:",
+        q: "Which is not a type of Pop up box available in JavaScript?",
         answers: {
-            a: 'Boolean',
-            b: 'String',
-            c: 'Alert',
-            d: 'Number'
+            a: 'Alert',
+            b: 'Display',
+            c: 'Prompt',
+            d: 'Confirm'
         },
-        correctAnswer: 'c'
+        correctAnswer: 'b'
     },
     {
-        q: "Common data types do not include:",
+        q: "What is the disadvantage of using innerHTML in JavaScript?",
         answers: {
-            a: 'Boolean',
-            b: 'String',
-            c: 'Alert',
-            d: 'Number'
+            a: 'Content is replaced everywhere',
+            b: 'The entire innerHTML content is re-parsed and build into elements, therefore its much slower',
+            c: 'The innerHTML does not provide validation and therefore we can potentially insert valid and broken HTML in the document and break it',
+            d: 'All of the above'
         },
-        correctAnswer: 'c'
+        correctAnswer: 'd'
     },
     {
         q: "Common data types do not include:",
@@ -91,19 +94,25 @@ var questions = [
         correctAnswer: 'c'
     },
 ];
+
 var score = 0;
 function askQuestions() {
-    for (var i = 0; i < questions.length; i++) {
-        var answer = confirm(questions[i].q);
-        if ((answer === true && questions[i].a === 't') ||
-            (answer === false && questions[i].a === 'f')
-        ) {
-            score++;
-        }
-        else (timeLeft - 15);
-    }
+    console.log ("I called quiz");
+    // for (var i = 0; i < questions.length; i++) {
+    //     var answer = prompt(questions[i].q);
+    //     if ((answers === correctAnswer)
+    //     ) {
+    //         alert("correct")
+    //         score++;
+    //     }
+    //     else{
+    //         timeLeft -= 10;
+    //     } 
+    // }
 }
 function viewScore() {
+    //display in quiz section instead
     alert("Your Score is: " + score);
 }
-startBtn.onclick = countdown;
+
+startBtn.addEventListener("click", countdown);
